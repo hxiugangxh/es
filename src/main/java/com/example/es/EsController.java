0c907f5b9755fb015e7e7cc58f012993604e834f
hxiugangxh <623145066@qq.com>
2018-04-12 00:16:04 +0800

    .

--- src/main/java/com/example/es/EsController.java
+++ src/main/java/com/example/es/EsController.java
@@ -4,11 +4,17 @@ package com.example.es;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -16,7 +22,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -107,5 +116,45 @@ public class EsController {
         }
     }
 
+    @PostMapping("/query/book/novel")
+    public ResponseEntity query(
+            @RequestParam(value = "author", required = false) String author,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "gtWordCount",defaultValue = "10") Integer gtWordCount
+    ) {
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+
+        if (author != null) {
+            boolQuery.must(QueryBuilders.matchQuery("author", author));
+        }
+        if (title != null) {
+            boolQuery.must(QueryBuilders.matchQuery("title", title));
+        }
+
+        RangeQueryBuilder rangeQuery = QueryBuilders.rangeQuery("word_count")
+                .from(gtWordCount).to(5000);
+
+        boolQuery.filter(rangeQuery);
+
+        SearchRequestBuilder searchRequestBuilder = this.client.prepareSearch("book")
+                .setTypes("novel")
+                .setSearchType(SearchType.QUERY_THEN_FETCH)
+                .setQuery(boolQuery)
+                .setFrom(0)
+                .setSize(10);
+
+        System.out.println(searchRequestBuilder);
+
+        SearchResponse searchResponse = searchRequestBuilder.get();
+        SearchHits hits = searchResponse.getHits();
+
+        List<Map<String, Object>> result = new ArrayList<>();
+
+        for (SearchHit hit : hits) {
+            result.add(hit.getSource());
+        }
+
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
 
 }
